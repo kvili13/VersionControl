42c1583aed09cfb8dfff246be722a7bc1ee422a4
Vilmos Kovács <vilmos.kovacs@stud.uni-corvinus.hu>
2019-12-06 18:31:27 +0100

    nyolcadik commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -53,5 +53,12 @@ namespace UserMaintenance
 
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string kivid = Convert.ToString(listBox1.SelectedValue);
+            var kiv = from x in users where x.ID.ToString() == kivid select x;
+            users.Remove(kiv.FirstOrDefault());
+        }
     }
 }
